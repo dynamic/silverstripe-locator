3134ab3a0f62243e1e9292a1c5f0e044eb9b5326
Jason Irish <jirish@dynamicdoes.com>
2013-04-27 14:42:45 -0500

    search form layout cleanup

--- code/Locator.php
+++ code/Locator.php
@@ -2,6 +2,30 @@
 
 class Locator extends Page {
 	
+	static $db = array(
+		'FilterByCategory' => 'Boolean',
+		'AutoGeocode' => 'Boolean',
+		'ModalWindow' => 'Boolean'
+	);
+	
+	static $defaults = array(
+		'FilterByCategory' => true,
+		'AutoGeocode' => true
+	);
+	
+	// variable holder for custom data source url
+	public static $datasource = null;
+	
+	// get custom data source url
+	public static function get_datasource() {
+		return self::$datasource;
+	}
+	
+	// allow custom data source url to be set via _config.php
+	static public function set_datasource($datasource) {
+		self::$datasource = $datasource;
+	}
+	
 	public static $singular_name = "Locator";
     public static $plural_name = "Locators";
     static $description = 'Find locations on a map';
@@ -9,19 +33,34 @@ class Locator extends Page {
     public function getCMSFields() {
 	    $fields = parent::getCMSFields();
 	    
-	    // Locations
+	    // Locations Grid Field
 		$config = GridFieldConfig_RecordEditor::create();	
 		//$config->addComponent(new GridFieldBulkEditingTools());
-	    
-		$LocationField = GridField::create("Locations", "Locations", Location::get(), $config);
-	    
-	    $fields->addFieldToTab("Root.Locations", $LocationField);
+	    $fields->addFieldToTab("Root.Locations", GridField::create("Locations", "Locations", Location::get(), $config));
+
+	    // Settings
+	    $fields->addFieldsToTab('Root.Settings', array(
+	    	HeaderField::create('DisplayOptions', 'Display Options', 3),
+	    	CheckboxField::create('AutoGeocode', 'Auto Geocode - Automatically filter map results based on user location'),
+	    	CheckboxField::create('FilterByCategory', 'Filter by Category - allow user to filter map results by category'),
+	    	CheckboxField::create('ModalWindow', 'Modal Window - Show Map results in a modal window'),
+	    	HeaderField::create('DataOptions', 'Data Options', 3),
+	    ));
 	    
 	    return $fields;
     }
     
     public function getAllCategories() {
 	    return LocationCategory::get();
+    }
+    
+    public function getDataLocation() {
+	    if (self::get_datasource()) {
+			$dataLocation = self::get_datasource();
+		} else {
+			$dataLocation = getDataLocation();
+		}
+		return $dataLocation;
     }
 	
 }
@@ -38,18 +77,37 @@ class Locator_Controller extends Page_Controller {
 		
 		Requirements::css('locator/css/map.css');
 
+		// map config based on user input in Settings tab
+		// AutoGeocode or Full Map
+		if ($this->AutoGeocode) {
+			$load = 'autoGeocode: true,
+			fullMapStart: false,';
+		} else {
+			$load = 'autoGeocode: false,
+			fullMapStart: true,';
+		}
+		
+		// in page or modal
+		if ($this->ModalWindow) {
+			$modal = 'modalWindow: true';
+		} else {
+			$modal = 'modalWindow: false';
+		}
+		
+		// data source
+		
+
 		// init map		
 		Requirements::customScript("
 			$(function($) {
 		      $('#map-container').storeLocator({
-		      	autoGeocode: true,
+		      	" . $load . "
 		      	dataLocation: '" . $this->Link() . "xml.xml',
 		      	listTemplatePath: '/locator/templates/location-list-description.html',
 		      	infowindowTemplatePath: '/locator/templates/infowindow-description.html',
 		      	originMarker: true,
-		      	fullMapStart: false,
-		      	slideMap: true,
-		      	modalWindow: false
+		      	" . $modal . ",
+		      	slideMap: false
 		      });
 		    });
 		");
@@ -58,9 +116,20 @@ class Locator_Controller extends Page_Controller {
 	
 	// Return all locations, render in XML file 
 	public function xml() {
-		return $this->customise(array(
-			'Locations' => Location::get()
-		))->renderWith('LocationXML');
-
+		if (self::get_datasource()) {
+			
+		    $service = new RestfulService(self::get_datasource()); 
+			
+			return $service->request()->getBody();
+			
+		} else {
+		
+			$Locations = Location::get();
+			
+			return $this->customise(array(
+				'Locations' => $Locations
+			))->renderWith('LocationXML');
+		}
+		
 	}	
 }
--- templates/Layout/Locator.ss
+++ templates/Layout/Locator.ss
@@ -5,19 +5,27 @@
 	<div id="form-container">
 	    <form id="user-location" method="post" action="#">
 	        <div id="form-input">
-	          	<label for="address">Enter Address or Zip Code:</label>
-	          	<input type="text" id="address" name="address" />
-	          	<label for="category">Filter by Category:</label>
-	          	<select id="category" name="category">
-	          		<option value="">All</option>
-	          		<% loop AllCategories %>
-	          			<option value="$Name.XML">$Name.XML</option>
-	          		<% end_loop %>
-	          	</select>	
+	          	<div class="field">
+		          	<label for="address">Enter Address or Zip Code:</label>
+		          	<input type="text" id="address" name="address" />
+	          	</div>
+	          	<div class="field">
+		          	<% if $FilterByCategory %>
+			          	<label for="category">Filter by Category:</label>
+			          	<select id="category" name="category">
+			          		<option value="">All</option>
+			          		<% loop AllCategories %>
+			          			<option value="$Name.XML">$Name.XML</option>
+			          		<% end_loop %>
+			          	</select>	
+		          	<% end_if %>
+		        </div>
 	         </div>
 	        
 	         <div id="submit-btn">
-	         	<input type="image" id="submit" name="submit" src="/locator/images/submit.jpg" alt="Submit" />
+	         	<div class="field">
+	         		<input type="image" id="submit" name="submit" src="/locator/images/submit.jpg" alt="Submit" />
+	         	</div>
 	         </div>
 	    </form>
 	</div>
