4de2ee90d0f8b78b330e9a5e3fac7fdfba1835c1
Jason Irish <jirish@dynamicagency.com>
2015-07-12 23:24:04 -0500

    import export sync
    Matched fields in LocationAdmin::getExportFields() with LocationCsvBulkLoader.
    
    Can now export easily from one Locator installation to another without data loss or reformatting column headers in CSV

--- code/LocationAdmin.php
+++ code/LocationAdmin.php
@@ -1,34 +1,39 @@
 <?php
 class LocationAdmin extends ModelAdmin {
-   	
-   	static $managed_models = array(
-   		'Location',
-   		'LocationCategory'
-   	);
-   	
-   	static $model_importers = array(
-      	'Location' => 'LocationCsvBulkLoader',
-      	'LocationCategory' => 'CsvBulkLoader'
-    );
-   
-	static $menu_title = 'Locator';
-    static $url_segment = 'locator';
 
-    public function getExportFields() {
-        return array(
-            'Title' => 'Name',
-            'Address' => 'Address',
-            'Suburb' => 'City',
-            'State' => 'State',
-			'Postcode' => 'Postal Code',
-			'Country' => 'Country',
-			'Website' => 'Website',
-			'Phone' => 'Phone',
-			'Fax' => 'Fax',
-			'EmailAddress' => 'Email Address',
-			'ShowInLocator' => 'Show',
-			'Lat' => 'Lat',
-			'Lng' => 'Lng'
-        );
+  static $managed_models = array(
+    'Location',
+    'LocationCategory'
+  );
+
+  static $model_importers = array(
+    'Location' => 'LocationCsvBulkLoader',
+    'LocationCategory' => 'CsvBulkLoader'
+  );
+
+  static $menu_title = 'Locator';
+  static $url_segment = 'locator';
+
+  public function getExportFields() {
+    if($this->modelClass == 'Location') {
+      return array(
+        'Title' => 'Name',
+        'Address' => 'Address',
+        'Suburb' => 'City',
+        'State' => 'State',
+        'Postcode' => 'Postcode',
+        'Country' => 'Country',
+        'Website' => 'Website',
+        'Phone' => 'Phone',
+        'Fax' => 'Fax',
+        'EmailAddress' => 'EmailAddress',
+        'Category.Name' => 'Category',
+        'ShowInLocator' => 'ShowInLocator',
+        'Featured' => 'Featured',
+        'Lat' => 'Lat',
+        'Lng' => 'Lng'
+      );
     }
+    return parent::getExportFields();
+  }
 }
