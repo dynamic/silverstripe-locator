a10390a31071f83a44dc86860705900b899ee16e
Jason Irish <jirish@dynamicagency.com>
2017-02-10 15:49:34 -0600

    LocationCategory - permissions
    closes #133

--- code/objects/LocationCategory.php
+++ code/objects/LocationCategory.php
@@ -9,7 +9,6 @@
  */
 class LocationCategory extends DataObject
 {
-
     /**
      * @var array
      */
@@ -35,6 +34,7 @@ class LocationCategory extends DataObject
      * @var string
      */
     private static $singular_name = 'Category';
+
     /**
      * @var string
      */
@@ -45,4 +45,64 @@ class LocationCategory extends DataObject
      */
     private static $default_sort = 'Name';
 
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+
+        $fields->removeByName([
+            'Locations',
+        ]);
+
+        if ($this->ID) {
+            // Locations
+            $config = GridFieldConfig_RelationEditor::create();
+            $config->removeComponentsByType('GridFieldAddExistingAutocompleter');
+            $config->addComponent(new GridFieldAddExistingSearchButton());
+            $config->removeComponentsByType('GridFieldAddNewButton');
+            $locations = $this->Locations();
+            $locationField = GridField::create('Locations', 'Locations', $locations, $config);
+
+            $fields->addFieldsToTab('Root.Locations', array(
+                $locationField,
+            ));
+        }
+
+        return $fields;
+    }
+
+    /**
+     * @param null|Member $member
+     * @return bool
+     */
+    public function canView($member = null)
+    {
+        return true;
+    }
+
+    /**
+     * @param null|Member $member
+     * @return bool|int
+     */
+    public function canEdit($member = null)
+    {
+        return Permission::check('Location_EDIT', 'any', $member);
+    }
+
+    /**
+     * @param null|Member $member
+     * @return bool|int
+     */
+    public function canDelete($member = null)
+    {
+        return Permission::check('Location_DELETE', 'any', $member);
+    }
+
+    /**
+     * @param null|Member $member
+     * @return bool|int
+     */
+    public function canCreate($member = null)
+    {
+        return Permission::check('Location_CREATE', 'any', $member);
+    }
 }
