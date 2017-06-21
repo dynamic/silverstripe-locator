55fc4d1e42056f91d9b709d2b4d5d4c3c9eb5a06
Jason Irish <jirish@dynamicagency.com>
2017-06-20 22:51:46 -0500

    bugfix - Location - correct namespace for VerstionedDataobject

--- code/objects/Location.php
+++ code/objects/Location.php
@@ -113,7 +113,7 @@ class Location extends DataObject implements PermissionProvider
      * @var array
      */
     private static $extensions = [
-        'Dynamic\ViewableDataObject\Extensions\VersionedDataObject',
+        'Heyday\VersionedDataObjects\VersionedDataObject',
     ];
 
     /**
