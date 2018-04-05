68e10b517405814900f9f47a27f814490e464991
mak001 <mak001@users.noreply.github.com>
2018-04-05 10:39:45 -0500

    Locations many categories (#182)
    * Fixed missing task extension

---
+++ tasks/LocationCategoryExtension.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Dynamic\Locator\Tasks;
+
+use Dynamic\Locator\LocationCategory;
+use SilverStripe\ORM\DataExtension;
+
+/**
+ * This is meant to only be applied during the upgrade task, not for normal use
+ *
+ * Class LocationCategoryExtension
+ * @package Dynamic\Locator\Tasks
+ */
+class LocationCategoryExtension extends DataExtension
+{
+    private static $has_one = [
+        'Category' => LocationCategory::class,
+    ];
+}
