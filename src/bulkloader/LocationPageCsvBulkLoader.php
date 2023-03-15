8629c58b02b843490e6de6b8071e58e593abe4d0
Jason Irish <jsirish@users.noreply.github.com>
2023-03-15 16:32:53 -0500

    Admin cleanup, implement CatalogAdmin (#237)

---
+++ src/bulkloader/LocationPageCsvBulkLoader.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Dynamic\Locator\Bulkloader;
+
+use Dynamic\Locator\Model\LocationCategory;
+use Dynamic\Locator\Page\LocationPage;
+use Dynamic\Locator\Page\Locator;
+use SilverStripe\Dev\CsvBulkLoader;
+use SilverStripe\Core\Convert;
+use SilverStripe\i18n\Data\Intl\IntlLocales;
+
+/**
+ * Class LocationCsvBulkLoader
+ * @package Dynamic\Locator
+ */
+class LocationPageCsvBulkLoader extends CsvBulkLoader
+{
+
+    /**
+     * @var array
+     */
+    public $columnMap = array(
+        'Name' => 'Title',
+        'EmailAddress' => 'Email',
+        'Categories' => '->getCategoryByName',
+        'Import_ID' => 'Import_ID',
+        'Country' => '->getCountryByName',
+    );
+
+    /**
+     * @var array
+     */
+    public $duplicateChecks = array(
+        'Import_ID' => 'Import_ID'
+    );
+
+    /**
+     * @param $val
+     * @return string|string[]|null
+     */
+    public function getEscape($val)
+    {
+        return preg_replace("/\r|\n/", "", $val);
+    }
+
+    /**
+     * @param $obj
+     * @param $val
+     * @param $record
+     * @throws \SilverStripe\ORM\ValidationException
+     */
+    public static function getCategoryByName(&$obj, $val, $record)
+    {
+        if ($val = Convert::raw2sql($val)) {
+            $parts = explode(', ', $val);
+
+            foreach ($parts as $part) {
+                $category = LocationCategory::get()->filter(array('Name' => $part))->first();
+                if (!$category) {
+                    $category = LocationCategory::create();
+                    $category->Name = $part;
+                    $category->write();
+                }
+                $obj->Categories()->add($category);
+            }
+        }
+    }
+
+    /**
+     * @param $obj
+     * @param $val
+     * @param $record
+     */
+    public function getCountryByName(&$obj, $val, $record)
+    {
+        $val = $this->getEscape($val);
+        $countries = IntlLocales::singleton()->getCountries();
+
+        if (strlen((string)$val) == 2) {
+            $val = strtolower($val);
+            if (array_key_exists($val, $countries)) {
+                $obj->Country = $val;
+            }
+        }
+
+        if (in_array($val, $countries)) {
+            $obj->Country = array_search($val, $countries);
+        }
+    }
+
+    /**
+     * @param array $record
+     * @param array $columnMap
+     * @param \SilverStripe\Dev\BulkLoader_Result $results
+     * @param bool $preview
+     * @return int|void
+     */
+    protected function processRecord($record, $columnMap, &$results, $preview = false)
+    {
+        $objID = parent::processRecord($record, $columnMap, $results, $preview = false);
+
+        $location = LocationPage::get()->byID($objID);
+        $location->ParentID = Locator::get()->first()->ID;
+        $location->publishSingle();
+    }
+}
